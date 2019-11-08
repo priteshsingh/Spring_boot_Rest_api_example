befb4e21a0d5f7711b110ecf0baefdcea4ec3827
Pritesh.Singh <Pritesh.Singh@MLBBT-106163.in.marlabs.com>
2019-11-08 15:33:37 +0530

    second commit

--- src/main/java/com/example/repo/ProductRepo.java
+++ src/main/java/com/example/repo/ProductRepo.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import com.example.model.Product;
 
+//Repository
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long>{
 
